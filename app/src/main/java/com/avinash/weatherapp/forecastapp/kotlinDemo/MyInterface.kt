6326734c8af6f769747ae57ae774173a6293e654
Avi29071991 <Avi29071991@users.noreply.github.com>
2018-02-07 11:37:38 +1100

    Update MyInterface.kt

--- app/src/main/java/com/avinash/weatherapp/forecastapp/kotlinDemo/MyInterface.kt
+++ app/src/main/java/com/avinash/weatherapp/forecastapp/kotlinDemo/MyInterface.kt
@@ -1,7 +1,7 @@
 package com.avinash.weatherapp.forecastapp.kotlinDemo
 
 /**
- * Created by L091735 on 5/02/2018.
+ * Created by Avinash Mandal on 5/02/2018.
  */
 interface MyInterface {
     fun setNameValue(value: String){
